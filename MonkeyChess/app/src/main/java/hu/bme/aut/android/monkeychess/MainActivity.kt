f00d18987a709bb686d7a254d3b6b1de91916f6c
Varesz <varga.adam011208@edu.bme.hu>
2023-03-13 16:37:01 +0100

    Project updated

---
+++ MonkeyChess/app/src/main/java/hu/bme/aut/android/monkeychess/MainActivity.kt
@@ -0,0 +1,92 @@
+package hu.bme.aut.android.monkeychess
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import hu.bme.aut.android.monkeychess.WelcomeScreen.WelcomeUI
+import hu.bme.aut.android.monkeychess.Login.LoginUI
+import hu.bme.aut.android.monkeychess.SplashScreen.SplashScreenUI
+import hu.bme.aut.android.monkeychess.ui.theme.MonkeChessTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MonkeChessTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ){
+                    MainActivityNavigation()
+                }
+            }
+        }
+    }
+    override fun onBackPressed() {
+        //finish()
+    }
+
+    @Composable
+    fun MainActivityNavigation(){
+        val navController = rememberNavController()
+        NavHost(navController = navController, startDestination = "splash_screen"){
+            composable("splash_screen"){
+                //SplashScreenUI().SplashScreen(navController)
+                TopAppBarWidget(navController = navController, content =  { SplashScreenUI().SplashScreen(navController)})
+            }
+            composable("welcome_screen"){
+                TopAppBarWidget(navController = navController, content =  { WelcomeUI().WelcomeScreenContent(navController)})
+            }
+
+            composable("login_screen"){
+               //LoginUI().LoginScreenContent()
+                TopAppBarWidget(navController = navController, content =  { LoginUI().LoginScreenContent()},bottomBar = {LoginUI().LoginButton()} )
+            }
+
+
+
+            composable("register_screen"){
+                //WelcomeUI().WelcomeScreenContent(navController)
+            }
+        }
+    }
+
+
+    /**
+     * TopAppBar
+     * */
+    @Composable
+    fun TopAppBarWidget(navController: NavController, content: @Composable () -> Unit={}, bottomBar: @Composable () -> Unit={} ) {
+        Scaffold(
+            topBar = {WelcomeScreenTopBar() },
+            content = {content()},
+            bottomBar = {bottomBar()}
+        )
+    }
+
+    @Composable
+    fun WelcomeScreenTopBar(){
+        TopAppBar(
+            title = {
+                Text(
+                    text = "MonkeyChess",
+                    color = MaterialTheme.colors.background
+                )
+            },
+            backgroundColor = MaterialTheme.colors.onBackground,
+            contentColor = MaterialTheme.colors.background
+        )
+    }
+
+
+}
+
---
+++ MonkeyChess/app/src/main/java/hu/bme/aut/android/monkeychess/Register/Register.kt
@@ -0,0 +1,28 @@
+package hu.bme.aut.android.monkeychess.Register
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import hu.bme.aut.android.monkeychess.ui.theme.MonkeChessTheme
+
+class Register : ComponentActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MonkeChessTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ){
+                    //MainActivityNavigation()
+                }
+            }
+        }
+    }
+}
